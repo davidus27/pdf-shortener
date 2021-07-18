d5e31134befef7775e5670da034c168bcaa09a23
davidus27 <davidisbest27@gmail.com>
2021-07-18 19:02:38 +0200

    make submit button capable of sending a props

--- src/components/SelectButton.tsx
+++ src/components/SelectButton.tsx
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 
 interface ButtonProps {
-  redirectTo: string;
-  onClick: any;
+  to: object | string;
+  onClick: () => void;
 }
 
 interface ButtonState {}
@@ -12,8 +12,8 @@ class SelectButton extends Component<ButtonProps, ButtonState> {
 
   render() {
     return (
-        <div className="submit position-absolute bottom-0 end-0">
-          <Link to={this.props.redirectTo}>
+        <div className="submit">
+          <Link to={this.props.to} >
             <button className="submit-btn" onClick={this.props.onClick}>
                 Submit
             </button>
