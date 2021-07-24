fa42a92007064619d5f3476160c1777b04b02642
davidus27 <davidisbest27@gmail.com>
2021-07-24 17:44:54 +0200

    removed extra routes

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import {
 
 import Home from './pages/Home';
 import Checkout from './pages/Checkout';
-import Loading from './components/Loading';
 import './styles/App.css';
 
 // import Footer from './components/Footer';
@@ -18,8 +17,6 @@ function App() {
       <Router>
         <Switch>
             <Route exact path="/Checkout" component={Checkout} />
-            <Route exact path="/Loading" component={Loading} />
-            { /* <Route path="/" component={NoDocumentFound} /> */ }
             <Route path="/" component={Home} />
         </Switch>
       </Router>
