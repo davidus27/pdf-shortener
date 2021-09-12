b485725373bd6455c9b19cc8293818bc4f7fb118
davidus27 <davidisbest27@gmail.com>
2021-09-12 23:04:15 +0200

    make loading more sexy

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -3,6 +3,9 @@ import Backdrop from '@material-ui/core/Backdrop';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import CloseIcon from '@material-ui/icons/Close';
 import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+
+
 
 const useStyles = makeStyles((theme) => ({
   backdrop: {
@@ -16,14 +19,17 @@ export default function SimpleBackdrop({ open, onCancel } : any) {
   const classes = useStyles();
 
   return (
-    <div>
       <Backdrop className={classes.backdrop} open={open}>
-        <CircularProgress color="inherit" />
-        <CloseIcon style={{ cursor: 'pointer', paddingBottom: '40px', paddingLeft: '10px'}} onClick={onCancel} ></CloseIcon>
-        <h1>
-          Loading...
-        </h1>
+        <Grid container spacing={0}>
+          <Grid item xs={12}>
+            <h1>Loading...</h1>
+          </Grid>
+
+          <Grid item xs={12}>
+            <CircularProgress color="inherit" />
+            <CloseIcon style={{ cursor: 'pointer', paddingBottom: '40px', paddingLeft: '10px'}} onClick={onCancel} ></CloseIcon>
+          </Grid>
+        </Grid>
       </Backdrop>
-    </div>
   );
 }
