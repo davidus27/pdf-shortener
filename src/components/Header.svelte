1d7656a7f29bb39f1a62be0a6f2c84245333b129
David Drobny <d.drobny@pan-net.eu>
2022-07-04 23:54:30 +0200

    initialize Header component

---
+++ src/components/Header.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import { Button, Breadcrumbs, Chip } from 'attractions';
+	import { HomeIcon, ArrowRightIcon } from 'svelte-feather-icons';
+
+    const items = [{ href: '/', text: 'Home' }];
+
+    
+</script>
+
+<div>
+    <Breadcrumbs {items}>
+        <div slot="item" let:item>
+          {#if item.href === '/'}
+            <Button href="/" round small>
+              <HomeIcon size="20" />
+            </Button>
+          {:else}
+            <Chip>{item.text}</Chip>
+          {/if}
+        </div>
+        <ArrowRightIcon slot="separator" size="20" />
+      </Breadcrumbs>
+</div>
