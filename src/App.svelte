ae796d3809710695168d23e418e2e73b0a9113f8
David Drobny <d.drobny@pan-net.eu>
2022-07-05 20:13:15 +0200

    added custom transition between components with added context for the breadcrumbs

--- src/App.svelte
+++ src/App.svelte
@@ -1,62 +1,123 @@
-<script lang="ts" >
-	import { Button, Tabs } from 'attractions';
-	// import { } from 'svelte-feather-icons';
-	
-	import Filter from './components/Filter.svelte';
-	import Footer from './components/Footer.svelte';
-	import Header from './components/Header.svelte';
-	import Processing from './components/Processing.svelte';
-	import Starter from './components/Starter.svelte';
-	import Warning from './components/Warning.svelte';
+<script lang="ts">
+  import { fade } from "svelte/transition";
+  import { Button } from "attractions";
+  import { ArrowLeftIcon, XIcon } from "svelte-feather-icons";
+
+  import Footer from "./components/Footer.svelte";
+  import Header from "./components/Header.svelte";
+  import Processing from "./components/Processing.svelte";
+  import Starter from "./components/Starter.svelte";
+  import Preview from "./components/Preview.svelte";
+
+  let files = [];
+  let breadcrumbsPath = [{ href: "/", text: "Home" }];
+
+  const views = [Starter, Processing, Preview];
+
+  const path = [
+    { href: "/", text: "Home" },
+    { href: "/processing", text: "Processing" },
+    { href: "/preview", text: "Preview" },
+  ];
+
+  let currentView = 0;
+  let viewComponent = views[currentView];
+
+  const updateView = () => {
+    viewComponent = views[currentView];
+    breadcrumbsPath = path.slice(0, currentView + 1);
+  };
+
+  const moveNext = () => {
+    // check if we are at the last view
+    if (currentView === views.length - 1) {
+      return;
+    }
+    currentView++;
+    updateView();
+  };
+
+  const movePrevious = () => {
+    // check if we are at the first view
+    if (currentView === 0) {
+      return;
+    }
+    currentView--;
+    updateView();
+  };
+
+  const reset = () => {
+    currentView = 0;
+    updateView();
+  };
 
 
+  const saveAlert = (e: any) => {
+    if (files.length == 0) {
+      return;
+    }
+    if(!e) e = window.event;
+    //e.cancelBubble is supported by IE - this will kill the bubbling process.
+    e.cancelBubble = true;
+    //This is displayed on the dialog box.
+    e.returnValue = 'Are you sure you want to leave? All the progress will be removed'; 
+    //e.stopPropagation works in Firefox.
+    if (e.stopPropagation) {
+        e.stopPropagation();
+        e.preventDefault();
+    }
+  }
+  // window.onbeforeunload = saveAlert; 
+  window.onbeforeprint = undefined;
 
-	let files = [];
-	let processing = false;
-	let selectedTab = 'Main dishes';
+  let props = {
+    files,
+    moveNext,
+  };
 
-	
 </script>
 
-
 <main>
+  <Header bind:items={breadcrumbsPath} />
 
+  <div id="viewport" on:outroend={updateView} transition:fade>
+    {#if currentView > 0}
+      <div class="buttons">
+        <Button on:click={movePrevious} round small>
+          <ArrowLeftIcon size="20" />
+        </Button>
+        <Button on:click={reset} round small>
+          <XIcon size="20" />
+        </Button>
+      </div>
+    {/if}
+    <svelte:component this={viewComponent} {...props} />
+  </div>
 
-	<Header />
-
-	{#if !processing}
-		<Starter on:files />
-		
-		<Button disabled={Boolean(files.length)} filled on:click={() => processing = true} >Submit</Button>
-		<Button on:click={() => console.log(files)} >Debug</Button>
-	{:else}
-
-		<Processing bind:files bind:processing />
-
-	
-		<!-- <Button outline>
-			<div id="pdf-viewer" />
-		</Button>
-		<Warning /> -->
-
-	{/if}	
-
-	<Footer />
-
+  <!-- <Footer /> -->
 </main>
 
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  /* set viewport to the flexbox */
+  #viewport {
+    height:100vh; 
+    margin:0;
+    min-height:50px;
+    display:flex; 
+    flex-direction:column;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
