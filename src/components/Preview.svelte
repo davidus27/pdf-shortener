da9018b8ee67b46cbb1ab49e469d3c0a77ac8e14
David Drobny <d.drobny@pan-net.eu>
2022-07-05 19:52:38 +0200

    initialize Preview component

---
+++ src/components/Preview.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  // import { Button, Breadcrumbs, Chip } from 'attractions';
+  // import { HomeIcon, ArrowRightIcon } from 'svelte-feather-icons';
+</script>
+
+<div>
+  <h1>Preview</h1>
+</div>
