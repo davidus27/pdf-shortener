a7371702d7aded34181c3cc916b89d50878f2217
David Drobny <d.drobny@pan-net.eu>
2022-07-05 20:10:44 +0200

    formatted Processing component

--- src/components/Filter.svelte
+++ src/components/Filter.svelte
@@ -1,104 +1,82 @@
 <script lang="ts">
   import {
     Button,
-    FileDropzone,
     FormField,
-    Dialog,
     Switch,
-    Checkbox,
-    Breadcrumbs,
-    Loading,
     Divider,
-    Modal,
     Slider,
     TextField,
     Autocomplete,
   } from "attractions";
 
-  import { PlusIcon, GithubIcon } from 'svelte-feather-icons';
-import { each } from "svelte/internal";
-
+  import { PlusIcon } from "svelte-feather-icons";
 
   let containsText = "";
   let linkClicked = false;
   let highlightsClicked = false;
   let imagesClicked = false;
   let pageIndexes = "";
-  
-  let ranges = [{
-        start: 0,
-        end: 9,
-      }];
+
+  let ranges = [
+    {
+      start: 0,
+      end: 9,
+    },
+  ];
 
   // define the sleep function for async/await
   function sleep(ms: number) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+    return new Promise((resolve) => setTimeout(resolve, ms));
   }
 
-
-  async function* getOptions(text) {
+  async function* getOptions(text: string) {
     await sleep(1000);
     yield [
-      { name: text, details: 'Optional' },
+      { name: text, details: "Optional" },
       { name: `it highlights the match: ${text}` },
     ];
   }
-
-
-
-
-
 </script>
 
-
-
-
 <div>
-
   <h1>Processing</h1>
   <h2>Find pages that contain...</h2>
 
-
-    <!-- <Button href="https://github.com/davidus27/pdf-shortener">
-        <GithubIcon size="25"/>
-    </Button>
-
-    <div>
-        <Button >
-            <GithubIcon size="25"/>
-        </Button>
-    </div> -->
-
-    <FormField name="Text:" optional >
-      <Autocomplete {getOptions} />
+  <FormField name="Text:" optional>
+    <Autocomplete {getOptions} />
   </FormField>
 
   <FormField name="Links:" errors={[linkClicked && "Not happy enough"]}>
     <Switch bind:value={linkClicked} />
   </FormField>
 
-  <FormField name="Highlights:" errors={[highlightsClicked && "Not happy enough"]}>
+  <FormField
+    name="Highlights:"
+    errors={[highlightsClicked && "Not happy enough"]}
+  >
     <Switch bind:value={highlightsClicked} />
   </FormField>
 
   <FormField name="Images:" errors={[imagesClicked && "Not happy enough"]}>
-    <Switch bind:value={imagesClicked} /> 
+    <Switch bind:value={imagesClicked} />
   </FormField>
 
-  <Divider text="OR"/>
+  <Divider text="OR" />
 
   <h2>Filter pages...</h2>
 
   <FormField name="Text:" optional>
     <TextField />
-    <Button on:click={() => {
-      ranges.push({
-        start: 0,
-        end: 100,
-      });
-      ranges = ranges
-    }}>
-      <PlusIcon size="25"/>
+    <Button
+      on:click={() => {
+        ranges.push({
+          start: 0,
+          end: 100,
+        });
+        ranges = ranges;
+      }}
+    >
+      <PlusIcon size="25" />
     </Button>
 
     {#each ranges as range}
@@ -111,7 +89,5 @@ import { each } from "svelte/internal";
         ticks={{ mode: "values", values: [1, 3, 5, 7, 9] }}
       />
     {/each}
-
-    
   </FormField>
 </div>
