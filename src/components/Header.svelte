2b8c499cbb06cc8839f8745869d340ebb3ff0fe6
David Drobny <d.drobny@pan-net.eu>
2022-07-05 19:53:55 +0200

    add Github Icon, Divider line and styling to the Header component

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,23 +1,63 @@
 <script lang="ts">
-    import { Button, Breadcrumbs, Chip } from 'attractions';
-	import { HomeIcon, ArrowRightIcon } from 'svelte-feather-icons';
+  import { Button, Breadcrumbs, Chip, Card, Divider } from "attractions";
+  import { HomeIcon, ArrowRightIcon, GithubIcon } from "svelte-feather-icons";
 
-    const items = [{ href: '/', text: 'Home' }];
-
-    
+  export let items: { href: string; text: string }[];
 </script>
 
-<div>
+<div class="navbar">
+  <div class="breadcrumbs">
     <Breadcrumbs {items}>
-        <div slot="item" let:item>
-          {#if item.href === '/'}
-            <Button href="/" round small>
-              <HomeIcon size="20" />
-            </Button>
-          {:else}
-            <Chip>{item.text}</Chip>
-          {/if}
-        </div>
-        <ArrowRightIcon slot="separator" size="20" />
-      </Breadcrumbs>
+      <div slot="item" let:item>
+        {#if item.href === "/"}
+          <Button href="/" round small>
+            PDF Shortner&nbsp;
+            <HomeIcon size="20" />
+          </Button>
+        {:else}
+          <Chip>{item.text}</Chip>
+        {/if}
+      </div>
+      <ArrowRightIcon slot="separator" size="20" />
+    </Breadcrumbs>
+  </div>
+  
+  <div class="github-button" >
+    <Button href="https://github.com/davidus27/pdf-shortener">
+      <GithubIcon size="25"/>
+    </Button>
+  </div>
 </div>
+
+
+<Divider />
+
+<style>
+
+  /* navbar will be alligned horizonarally at the same level */
+  .navbar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0.5rem 1rem;
+  }
+
+  /*
+    breadcrumbs will be alligned to the left corner
+    github button will be alligned to the right corner
+  */
+  .breadcrumbs {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  .github-button {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+  }
+
+
+  
+
+</style>
