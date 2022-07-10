df6ded5526d8a8217e5ed22c4613932f3c7db885
David Drobny <d.drobny@pan-net.eu>
2022-07-10 22:28:52 +0200

    add renderPages to the PdfViewer

--- src/pdf.ts
+++ src/pdf.ts
@@ -38,7 +38,7 @@ const prepareDocumentProxy = async (files: Array<File>) => {
     return pdfTasks;
 }
 
-const processAllDocuments = async (files: any): Promise<Array<number>> {
+const processAllDocuments = async (files: any): Promise<Array<number>> => {
     const pdfDocuments = await prepareDocumentProxy(files);
 
     const foundHighlight = [];
@@ -59,39 +59,70 @@ const processAllDocuments = async (files: any): Promise<Array<number>> {
                     foundHighlight.push(pageIndex);
                 }
             }
-            // console.log(annotations);
-
         }
-        // console.log(pdfDocuments[fileIndex]);
     }
     return foundHighlight;
 }
 
+// define class for pdf viewer
+class PdfViewer {
+    private files: Array<File>;
+    private pdfDocuments: Array<any>;
+    // private indexes: any;
+    // private scale: number;
 
-const renderPage = (pdf: any, pageNumber: number, canvas: any, scale: number) => {
-    pdf.getPage(pageNumber).then(function(page: any) {
-      const viewport = page.getViewport({ scale: scale });
-      canvas.height = viewport.height;
-      canvas.width = viewport.width;          
-      page.render({canvasContext: canvas.getContext('2d'), viewport: viewport});
-    });
-}
+    constructor(files: Array<File>, indexes: any, scale = 0.4) {
+        this.files = files;
+        // this.indexes = indexes;
+        // this.scale = scale;
+    }
 
-const renderPages = (document: any, pdfDocument: any, filter: Array<number>, scale = 0.4) => {
-    const viewer = document.getElementById('pdf-viewer');
-    
-    for(let page = 1; page <= pdfDocument.numPages; page++) {
-        // render only pages that are in the filter
-        if(filter.includes(page)) {
-            const canvas = document.createElement("canvas");    
-            canvas.className = 'pdf-page-canvas';         
-            viewer.appendChild(canvas);            
-            renderPage(pdfDocument, page, canvas, scale);
+    public async load() {
+        this.pdfDocuments = await prepareDocumentProxy(this.files);
+    }
+
+    // get the pdf document indexes based on the indexes
+    public getPdfDocumentIndexes() {
+        return processAllDocuments(this.files);
+    }
+
+    private renderPage(pdf: any, pageNumber: number, canvas: any, scale: number) {
+        pdf.getPage(pageNumber).then(function(page: any) {
+          const viewport = page.getViewport({ scale: scale });
+          canvas.height = viewport.height;
+          canvas.width = viewport.width;          
+          page.render({canvasContext: canvas.getContext('2d'), viewport: viewport});
+        });
+    }
+
+    public renderPages(filter: Array<number>, scale = 0.4) {
+        const viewer = document.getElementById('pdf-viewer');
+
+        // remove all children
+        while(viewer.firstChild) {
+            viewer.removeChild(viewer.firstChild);
         }
+
+        // increase the filter by 1 for each page
+        const increased_filter = filter.map((pageNumber) => pageNumber + 1);
+
+        for(let page = 1; page <= this.pdfDocuments[0].numPages; page++) {
+            // render only pages that are in the filter
+            if(increased_filter.includes(page)) {
+                const canvas = document.createElement("canvas");    
+                canvas.className = 'pdf-page-canvas';
+                canvas.setAttribute('style', 'padding: 20px;');
+                viewer.appendChild(canvas);            
+                this.renderPage(this.pdfDocuments[0], page, canvas, scale);
+            }
+        }
+    }
+    
+    // get page count of a pdf document by index
+    public getPageCount(index: number = 0) {
+        return this.pdfDocuments[index].numPages;
     }
 }
 
 
-
-
-export { processAllDocuments, prepareDocumentProxy, renderPages }; 
+export { PdfViewer }; 
