99a05c4290959d78b37e98c0a448a0b1207a2184
davidus27 <davidisbest27@gmail.com>
2022-08-23 18:13:05 +0200

    add input check for the pageRange
    It needs to be in a {number}-{number},{number} format

--- src/components/PageRange.svelte
+++ src/components/PageRange.svelte
@@ -7,6 +7,57 @@
   let ranges = [];
   export let textRange = "";
 
+  const textRangeIsCorrect = (textRange: string, pageCount: number) => {
+    // ignore the testing when empty
+    if (textRange.length === 0) {
+      return true;
+    }
+
+    // check for correct format: 
+    // {number}-{number}, {number}, {number}-{number},
+    // we don't want to allow any other characters
+    // we don't want the comma at the end so it's going to be added later
+    const foundCorrectForm = new RegExp(/^([0-9]+(-[0-9]+){0,1},)*$/g).test(textRange + ',');
+
+    // separate the ranges
+    if (!foundCorrectForm) {
+      return false;
+    }
+    const ranges = textRange.split(",");
+    // check if the ranges are correct
+    for (let i = 0; i < ranges.length; i++) {
+      const range = ranges[i];
+      // separate range to two numbers
+      const rangeNumbers = range.split("-");
+
+      // if rangeNumbers has only one element, it's a single number
+      if (rangeNumbers.length === 1) {
+        const number = parseInt(rangeNumbers[0]);
+        if (number < 1 || number > pageCount) {
+          return false;
+        }
+      } else if (rangeNumbers.length !== 2) {
+        return false;
+      }
+      // check if the numbers are correct
+      const firstNumber = parseInt(rangeNumbers[0]);
+      const secondNumber = parseInt(rangeNumbers[1]);
+      if (firstNumber > secondNumber) {
+        return false;
+      }
+
+      if (firstNumber < 1 || secondNumber > pageCount) {
+        return false;
+      }
+
+      // I think we can ignore this one
+      // if (firstNumber === secondNumber) {
+      //   return false;
+      // }
+    }
+    return true;
+  }
+
   // function that appends text range from slider values
   const updateTextRange = () => {
     let textRange = "";
@@ -52,6 +103,7 @@
   <FormField 
     name="Page ranges:"
     help="Enter page ranges separated by commas. Or use interactive slider by selecting 'Add new range'."
+    errors={[ !textRangeIsCorrect(textRange, pageCount) && 'Range in not in a correct format. Note: It can\'t exceed the boundaries of the page. Ex. 1-3,5,7-10' ]}
     >
     <TextField class="page-range-input" placeholder="e.g. 1-5,8,11-13" bind:value={textRange} />
 
